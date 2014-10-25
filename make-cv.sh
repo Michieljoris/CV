a535490a57becd29405da57c6b3e15ff42c62b18
Michiel van Oosten <mail@axion5.net>
2014-10-25 13:58:07 +0200

    Update

--- make-cv.sh
+++ make-cv.sh
@@ -4,5 +4,7 @@ echo "Creating resume.pdf and cover-letter.pdf and merging them.."
 (cd ./moderncv && pdflatex resume.tex && pdflatex cover-letter.tex)
 echo "Merging pdfs and copying to ./cv.pdf"
 pdfunite moderncv/cover-letter.pdf moderncv/resume.pdf cv.pdf
+echo "Opening cv.pdf"
+okular cv.pdf
 
 echo "Done."
