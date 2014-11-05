426aa131f93f6a8d4ec9ac9536807f9ac10e99bf
Michiel van Oosten <mail@axion5.net>
2014-11-05 21:20:01 +0100

    update

--- make-cv.sh
+++ make-cv.sh
@@ -1,11 +1,14 @@
 sed '1,28d' cover-letter-body.tex | sed '$d' > content.tex
 
-cat cover-letter-top.tex content.tex cover-letter-bottom.tex > cover-letter.tex
+cat cover-letter-top1.tex personal-data.tex cover-letter-top2.tex \
+content.tex cover-letter-bottom.tex > cover-letter.tex
+
+cat resume1.tex personal-data.tex resume2.tex > resume.tex
 
 rm content.tex
 
 echo "Copying resume.tex and cover-letter.tex to ./moderncv.."
-cp resume.tex cover-letter.tex ./moderncv
+mv resume.tex cover-letter.tex ./moderncv
 echo "Creating resume.pdf and cover-letter.pdf and merging them.."
 (cd ./moderncv && pdflatex resume.tex && pdflatex cover-letter.tex)
 echo "Merging pdfs and copying to ./cv.pdf"
