c0ba84dec761601ca07965ccac695029737a6957
Michiel van Oosten <mail@axion5.net>
2014-11-11 18:33:43 +0100

    Sync Tue Nov 11 2014 18:33:43 GMT+0100 (CET)

--- make-cv.sh
+++ make-cv.sh
@@ -13,6 +13,8 @@ echo "Creating resume.pdf and cover-letter.pdf and merging them.."
 (cd ./moderncv && pdflatex resume.tex && pdflatex cover-letter.tex)
 echo "Merging pdfs and copying to ./cv.pdf"
 pdfunite moderncv/cover-letter.pdf moderncv/resume.pdf cv.pdf
+echo "Copying to proper name"
+cp cv.pdf michiel_van_oosten_cv.pdf
 echo "Opening cv.pdf"
 okular cv.pdf
 
